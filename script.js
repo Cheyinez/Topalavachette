9dbabc7a20c1c4eee0eeed94c239f382f947cac3
AntoineRouillon <32706803+AntoineRouillon@users.noreply.github.com>
2017-10-17 17:15:30 +0200

    jour 2

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+    $("#bloc1").click(function(){
+        $("#panel1").slideToggle("slow");
+    });
+
+    $("#bloc2").click(function(){
+        $("#panel2").slideToggle("slow");
+    });
+
+    $("#bloc3").click(function(){
+        $("#panel3").slideToggle("slow");
+    });
+});
