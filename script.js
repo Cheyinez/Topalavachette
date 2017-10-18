c9a87cd2eb0689794ef127ca17bdd52967c97237
AntoineRouillon <32706803+AntoineRouillon@users.noreply.github.com>
2017-10-18 18:00:55 +0200

    jour3

--- script.js
+++ script.js
@@ -11,4 +11,16 @@ $(document).ready(function(){
     $("#bloc3").click(function(){
         $("#panel3").slideToggle("slow");
     });
+
+    $.getJSON("https://api.airtable.com/v0/appGUZh7thZxapx1y/musics?api_key=keyPrSeGbFB4cWNqf",
+    					function(data) {
+                console.log(data);
+                console.log(data.records[0].fields.likes);
+              	// fonction de succès
+                // j'ai envie ici d'afficher le contenu de data dans la balise article de mon site
+                // par défault, ce que me renvoie la fonction getJSON est un tableau/une liste qui contient tous les users
+                // pour l'afficher sur mon site, je dois le transformer en chaine de caractère, c'est à dire en texte brut, c'est le rôle de la fonction JSON.stringify
+                // Ensuite, je peux insérer ma variable data_str quelque part sur mon site, ici dans ma balise article
+    					});
+
 });
